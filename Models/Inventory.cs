b20f32032dc6c4c6de9b7fcd1b95f6c92b43f966
JBellows-git <jbellows212@gmail.com>
2021-11-09 15:13:46 -0600

    added Iventory class for store

---
+++ Models/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace AspGroupProject.Models
+{
+
+	public class Inventory
+	{
+		public int InventoryNumber { get; set; }
+		public string InventoryName { get; set; }
+		public string InventoryDescription { get; set; }
+		public int InventoryStock { get; set; }
+		public double IventoryPrice { get; set; }
+
+	}
+}
