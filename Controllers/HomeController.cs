163ec029a40d6264ff5c58df4b5fa43eff4d1eb6
JBellows-git <jbellows212@gmail.com>
2021-11-11 15:52:03 -0600

    added Admin Controller and page.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,6 +16,11 @@ namespace GroupProject.Controllers
             return View();
         }
 
+        public IActionResult Store()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
---
+++ Controllers/RoleAdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GroupProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace GroupProject.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RoleAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
