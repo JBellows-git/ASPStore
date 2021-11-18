3bc3ca0dc22a1f4a2319747d6aaceb89a6f383ee
AnthonyLacy <Huskeranthony@gmail.com>
2021-11-18 15:13:37 -0600

    code to run store

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,5 +1,8 @@
-﻿using GroupProject.Models;
+﻿using GroupProject.Data;
+using GroupProject.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +12,24 @@ namespace GroupProject.Controllers
 {
     public class ProductsController : Controller
     {
-        public async Task<IActionResult> Create()
+        private readonly ApplicationDbContext _context;
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public ProductsController(ApplicationDbContext context,
+                                  IHostingEnvironment hostingEnvironment)
         {
-            Inventory newInventory = new Inventory
-            return View();
+            _context = context;
+            this.hostingEnvironment = hostingEnvironment;
         }
+
+        public async Task<IActionResult> Store()
+        {
+            return View(await _context.Inventories.ToListAsync());
+        }
+        //public async Task<IActionResult> Create()
+        //{
+            //Inventory newInventory = new Inventory;
+            //return View();
+        //}
     }
 }
