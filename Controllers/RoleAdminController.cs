22dac8a40fcbe92a59c91cb67eb18325c1f3ebfb
JBellows-git <jbellows212@gmail.com>
2021-11-30 15:51:13 -0600

    Admin Customer List now works. Working on Validation for Customer will then apply to Inventory

--- Controllers/RoleAdminController.cs
+++ Controllers/RoleAdminController.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GroupProject.Data;
 using GroupProject.Models;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
 
 namespace GroupProject.Controllers
 {
@@ -13,9 +17,23 @@ namespace GroupProject.Controllers
     //[Authorize(Roles = "Admin")]
     public class RoleAdminController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public RoleAdminController(ApplicationDbContext context, IHostingEnvironment hostingEnvironment)
+        {
+            _context = context;
+            this.hostingEnvironment = hostingEnvironment;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> CustomerList()
+        {
+            return View(await _context.Customers.ToListAsync());
         }
     }
 }
